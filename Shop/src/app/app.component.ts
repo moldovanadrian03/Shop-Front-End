f32aca078d7f167e4ae8f1f13e009ffaec0d3315
Adrian <moldovan.adrian.alexandru03@gmail.com>
2023-05-07 21:40:15 +0300

    11. Inject the ProductService in the AppComponent, use the getProducts method and store the products in the products property.

--- Shop/src/app/app.component.ts
+++ Shop/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from './Services/product.service';
+import { Product } from './product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Shop';
+  products: Product[] | undefined;
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe(products => {
+      console.log("Products list: ");
+      this.products = products;
+    });
+  }
+
 }
