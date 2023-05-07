83579acaa055e170bc9a159549e45f1c8a2a5303
Adrian <moldovan.adrian.alexandru03@gmail.com>
2023-05-07 20:45:11 +0300

    4. Create a product interface with title, description, price, available, imageLink and define it as a type for the product property on the ProductComponent, use the interface where needed.

--- Shop/src/app/product/product.component.ts
+++ Shop/src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-product',
@@ -6,5 +7,5 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent {
-  @Input() product: any;
+  @Input() product: Product | undefined;
 }
