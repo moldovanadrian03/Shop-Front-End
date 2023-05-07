e8a78ac9ca35e34c15677cdd7dc3a61cd534b902
Adrian <moldovan.adrian.alexandru03@gmail.com>
2023-05-07 21:25:44 +0300

    10. Create a ProductService, add a getProducts method that should use the HttpClient to create a GET request and fetch the products from our mock server, the method must return an Observable<Product[]>  (so you don’t need to store the products in the ProductService).

---
+++ Shop/src/app/Services/product.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  baseURL = "http://localhost:2000/products";
+
+  readonly httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+    })
+  };
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProducts(): Observable<Product[]> {
+    console.log("Server Products: ");
+    return this.httpClient.get<Product[]>(this.baseURL, this.httpOptions);
+  }
+
+}
--- Shop/src/app/app.module.ts
+++ Shop/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,6 +14,7 @@ import { ProductComponent } from './product/product.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule
 
   ],
   providers: [],
